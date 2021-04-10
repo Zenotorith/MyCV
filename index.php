1ae256668d54e1aabc2e7e4c8cf0f5b8db46164d
Zenotorith <65653624+Zenotorith@users.noreply.github.com>
2021-04-10 07:03:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
         <div class="nav-bar">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#"><img src="" alt=""></a>
+              <a class="navbar-brand" href="#"><img src="img/Harry-X-logo.png" alt=""></a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fa fa-bars" aria-hidden="true"></i>
               </button>
